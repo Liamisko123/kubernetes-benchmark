a1d8d014449bb4ecd8161da33c430cafe86fd3bc
liamisko123 <liamspach@gmail.com>
2026-03-09 00:18:06 +0100

    Converted pod to JOB execution so we can put actual load onto the pods

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const os = require('os');
+const process = require('node:process');
 
 const port = 8080;
 const podName = process.env.HOSTNAME || "Neznámy Pod";
+let timeRemaining = 100;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -12,6 +14,7 @@ const server = http.createServer((req, res) => {
     <h1>Ahoj z Edge!</h1>
     <p>Aktuálne ti odpovedá Pod: <b>${podName}</b></p>
     <p>Moja interná IP: <b>${os.networkInterfaces().eth0?.[0].address || 'unknown'}</b></p>
+    <p>Zostavajuci cas bezania: ${timeRemaining}</p>
     <hr>
     <small>Bežím na Node.js verzii: ${process.version}</small>
   `;
@@ -22,3 +25,14 @@ const server = http.createServer((req, res) => {
 server.listen(port, '0.0.0.0', () => {
   console.log(`Server beží na porte ${port}`);
 });
+
+const timer = setInterval(() => {
+  timeRemaining -= 0.1;
+  if (timeRemaining <= 0) {
+    console.log("Čas vypršal. Vypínam server...");
+    clearInterval(timer);
+    server.close(() => {
+      process.exit(0);
+    });
+  }
+}, 100);
