45c156a3aa2b0cea3c684dd14fe2d9163c19e1df
Liamisko123 <liamspach@gmail.com>
2026-04-17 12:53:39 +0000

    initial k3s

--- cl2/run_test_next.sh
+++ cl2/run_test_next.sh
@@ -1,1 +1,5 @@
-./clusterloader2 --kubeconfig=$KUBECONFIG --testconfig=./cl2_config.yaml --provider=skeleton --enable-prometheus-server=true --tear-down-prometheus-server=false --v=6 --report-dir=./reports
+for i in {1..10}
+do
+	echo "Spúšťam test číslo: $i"
+	./clusterloader2 --kubeconfig=$KUBECONFIG --testconfig=./cl2_config.yaml --provider=skeleton --enable-prometheus-server=true --tear-down-prometheus-server=false --v=6 --report-dir=./reports
+done
